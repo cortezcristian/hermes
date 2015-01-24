a4fcb15abb359d8c5fec241e499a43aeffc8bdb5
Cristian Cortez <cortez.cristian@gmail.com>
2015-01-24 18:48:54 -0300

    Adding chat service

--- public/scripts/site/controllers/main.js
+++ public/scripts/site/controllers/main.js
@@ -8,6 +8,11 @@
  * Controller of the anyandgoApp
  */
 angular.module('anyandgoApp')
-  .controller('MainCtrl', function ($scope) {
+  .controller('MainCtrl', function ($scope, ChatService) {
+    $scope.chatmsg = '';
     $scope.anyandgoversion = 'v0.1';
+    $scope.sendPrivateChat = function(){
+        console.log($scope.chatmsg);    
+        ChatService.sendChat('userto', $scope.chatmsg);
+    };
   });
---
+++ public/scripts/site/services/chatservice.js
@@ -0,0 +1,42 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name anyandgoApp.controller:MainCtrl
+ * @description
+ * # ChatService
+ * Service of the anyandgoApp
+ */
+angular.module('anyandgoApp')
+  .factory('ChatService', function ($http) {
+     var req = function(method, path, data){
+        return $http({
+            method: method,
+            url: '.'+path,
+            data: data
+        }).then(function(res){
+            return res.data;
+        });
+     }
+     
+     return {
+         get: function(path, data){
+             return req('GET', path, data);
+         },
+         post: function(path, data){
+             return req('POST', path, data);
+         },
+         put: function(path, data){
+             return req('PUT', path, data);
+         },
+         delete: function(path, data){
+             return req('DELETE', path, data);
+         },
+         sendChat: function(userto, msg){
+             return req('POST', '/services/send/private/chat/', {
+                 userto: userto,
+                 msg: msg
+             });
+         }
+     }; 
+  });

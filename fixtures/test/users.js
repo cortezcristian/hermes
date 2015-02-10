3ea49112dd3cf9c758793c2ba39b32730f6f610e
Cristian Cortez <cortez.cristian@gmail.com>
2015-02-10 12:35:21 -0300

    Change fixtures to a different folder

--- fixtures/test/users.js
+++ fixtures/test/users.js
@@ -1,7 +1,5 @@
-/*
 module.exports.User = [
         { email: "user@user.com", password: "123456", avatar: '/photos/user.png' } 
         , { email: "demo@demo.com", password: "123456", avatar: '/photos/demo.png' } 
         , { email: "cortez.cristian@gmail.com", password: "12345678", avatar: '/photos/crisboot.png' } 
 ];
-*/

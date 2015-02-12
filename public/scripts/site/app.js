28054bdd4b6b5815924ccc338cdbca60fac00689
Cristian Cortez <cortez.cristian@gmail.com>
2015-02-12 02:01:58 -0300

    Adding pre-loader

--- public/scripts/site/app.js
+++ public/scripts/site/app.js
@@ -44,5 +44,34 @@ angular
       .otherwise({
         redirectTo: '/'
       });
-  });
+  }).run(function ($rootScope, $location,$route, $timeout) {
+
+    $rootScope.config = {};
+    $rootScope.config.app_url = $location.url();
+    $rootScope.config.app_path = $location.path();
+    $rootScope.layout = {};
+    $rootScope.layout.loading = false;
+
+    $rootScope.$on('$routeChangeStart', function () {
+        console.log('$routeChangeStart');
+        //show loading gif
+        $timeout(function(){
+          $rootScope.layout.loading = true;          
+        });
+    });
+    $rootScope.$on('$routeChangeSuccess', function () {
+        console.log('$routeChangeSuccess');
+        //hide loading gif
+        $timeout(function(){
+          $rootScope.layout.loading = false;
+        }, 200);
+    });
+    $rootScope.$on('$routeChangeError', function () {
+
+        //hide loading gif
+        console.log('error');
+        $rootScope.layout.loading = false;
+
+    });
+});;
 

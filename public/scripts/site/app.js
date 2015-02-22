564d2228b81320b6af27fe78e6ad5770ef97d869
Cristian Cortez <cortez.cristian@gmail.com>
2015-02-22 02:22:41 -0300

    Collapse sidebar

--- public/scripts/site/app.js
+++ public/scripts/site/app.js
@@ -1,13 +1,20 @@
 'use strict';
-/*
+
 $(document).ready(function(){
-    $('#side-menu').metisMenu();
-    $('head').append('<link rel="stylesheet" type="text/css" href="http://visionmedia.github.io/mocha/example/mocha.css">');
-    $.get('/tasks/test', function(data){
-        $('.main-con .panel').html(data);
+    $('a.collapser').click(function(e){
+        e.preventDefault();
+        e.stopPropagation();
+        $("#user-panel").toggleClass('do-collapse');
+        if($("#user-panel").hasClass('do-collapse')){
+            $("#side").width('40px');
+            $("#content").width(($(window).width()-40)+'px');
+        } else {
+            $("#side").width('');
+            $("#content").width('');
+        }
     });
 });
-*/
+
 /**
  * @ngdoc overview
  * @name anyandgoApp
